5db81af0324d30b4a0c50eb882e501dac2a90034
janeiroday-gif <janeiroday@hotmail.com>
2026-09-11 13:22:35 +0200

    Fix server

--- server.js
+++ server.js
@@ -2,19 +2,33 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
+
 const PORT = process.env.PORT || 3000;
 
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.post("/convert", async (req, res) => {
+app.post("/convert", (req, res) => {
   const { url } = req.body;
 
   if (!url) {
     return res.status(400).json({
-      error: 
+      error: "Vul eerst een YouTube-link in."
+    });
+  }
+
+  res.json({
+    message: "Link ontvangen! De converter wordt binnenkort toegevoegd.",
+    url: url
+  });
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`MP3Fast draait op poort ${PORT}`);
+});
